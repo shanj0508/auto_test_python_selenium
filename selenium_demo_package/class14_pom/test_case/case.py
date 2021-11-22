c7c3299525d5f953f4fd463ef82bd72f5a120f2a
shanj <605284854@qq.com>
2021-11-22 23:05:23 +0800

    'pom+ddt+yaml'

--- selenium_demo_package/class14_pom/test_case/case.py
+++ selenium_demo_package/class14_pom/test_case/case.py
@@ -2,8 +2,8 @@
     测试用例类：所有的测试代码都在该类实现
 '''
 import unittest
-from time import sleep
 
+from ddt import ddt, file_data
 from selenium import webdriver
 
 from selenium_demo_package.class14_pom.page_object.cart_page import CartPage
@@ -11,6 +11,7 @@ from selenium_demo_package.class14_pom.page_object.login_page import LoginPage
 from selenium_demo_package.class14_pom.page_object.phon_product_page import PhonePage
 
 
+@ddt
 class Case(unittest.TestCase):
     # 前置条件
     @classmethod
@@ -24,29 +25,14 @@ class Case(unittest.TestCase):
     def tearDownClass(cls) -> None:
         cls.driver.quit()
 
-    # 用例
-    # def test_shopxo(self):
-    #     user = 'shanjing'
-    #     pwd = 'shanjing@.000'
-    #     self.lp.login(user, pwd)
-    #     count = '4'
-    #     self.pp.add_cart(count)
-    #     status = cp.cart_info(count)
-    #     print('执行成功')
-    #     print(status)
-    #     sleep(5)
-    #     self.driver.quit()
-    # self.assertTrue(status)
-
     # 登录
-    def test_01_login(self):
-        user = 'shanjing'
-        pwd = 'shanjing@.000'
+    @file_data('../data/login.yaml')
+    def test_01_login(self, user, pwd):
         self.lp.login(user, pwd)
 
     # 添加商品
-    def test_02_add_cart(self):
-        count = '4'
+    @file_data('../data/add_cart.yaml')
+    def test_02_add_cart(self, count):
         self.pp.add_cart(count)
 
     # 购物车校验
