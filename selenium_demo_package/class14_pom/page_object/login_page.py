526c690b095faeec54f60513e2e421577bbd091a
shanj <605284854@qq.com>
2021-11-22 19:25:09 +0800

    '基本的LoginPage的设计与实现'

---
+++ selenium_demo_package/class14_pom/page_object/login_page.py
@@ -0,0 +1,42 @@
+'''
+    LoginPage：登录页，只为系统的登录业务而生成的页面对象。
+'''
+from selenium import webdriver
+
+from selenium_demo_package.class14_pom.base_page.base_page import BasePage
+
+
+# 登录页
+class LoginPage(BasePage):
+    '''
+        页面对象类的模板：
+            1.url
+            2.关键元素
+            3.行为
+    '''
+    # url
+    url = 'http://39.98.138.157/shopxo/index.php?s=/index/user/logininfo.html'
+
+    # 关键元素
+    username = ('name', 'accounts')
+    password = ('name', 'pwd')
+    login_button = ('xpath', '//button[text()="登录"]')
+
+    # 行为
+    def login(self, user, pwd):
+        # 访问登录页
+        self.visit(self.url)
+        # 输入账号和密码
+        self.input(self.username, user)
+        self.input(self.password, pwd)
+        # 点击登录按钮，实现登录操作
+        self.click(self.login_button)
+
+
+# 调试
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    user = 'shanjing'
+    pwd = 'shanjing@.000'
+    lp = LoginPage(driver)
+    lp.login(user, pwd)
