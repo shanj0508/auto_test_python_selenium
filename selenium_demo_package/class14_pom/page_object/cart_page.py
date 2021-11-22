630337df71bb6f64d95cc5cff400d161f422612d
shanj <605284854@qq.com>
2021-11-22 20:41:20 +0800

    '购物车页面'

---
+++ selenium_demo_package/class14_pom/page_object/cart_page.py
@@ -0,0 +1,17 @@
+'''
+    CartPage：购物车页面，校验商品是否添加成功
+'''
+from selenium_demo_package.class14_pom.base_page.base_page import BasePage
+
+
+# 购物车页面
+class CartPage(BasePage):
+    # url
+    url = 'http://39.98.138.157/shopxo/index.php?s=/index/cart/index.html'
+    # 关键元素
+    goods = ('xpath', '//div[@class="goods-base"]')
+
+    # 行为:校验商品是否存在
+    def cart_info(self):
+        self.visit(self.url)
+        self.wait(self.goods)
